878524cd08f55aaa77aa78afb6beb3e3fa7bbf01
jjtechcctv <jjtechccis2025@gmail.com>
2026-07-04 12:45:41 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log("JJTech Website Loaded");
+function sendMail(event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let service = document.getElementById("service").value;
+  let message = document.getElementById("message").value;
+
+  let mailBody =
+`JJTech Booking Request:
+
+Name: ${name}
+Phone: ${phone}
+Service: ${service}
+Message: ${message}`;
+
+  window.location.href =
+    `mailto:jjtechccis@gmail.com?subject=JJTech Booking Request&body=${encodeURIComponent(mailBody)}`;
+}
